8bb0c0c14dcd226a2aafad28075f8486514e7b07
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 18:31:24 +0600

    Added bg overlay to home hero section

--- sections/home-page/hero-section/HeroSection.tsx
+++ sections/home-page/hero-section/HeroSection.tsx
@@ -1,7 +1,12 @@
 import '@/sections/home-page/hero-section/hero-section.css';
 
 const HeroSection = () => {
-  return <section className='hero-section'>HeroSection</section>;
+  return (
+    <section className='hero-section'>
+      <div className='home-hero-img-overlay' />
+      <div className='hero-section-content'>Hero Section</div>
+    </section>
+  );
 };
 
 export default HeroSection;
