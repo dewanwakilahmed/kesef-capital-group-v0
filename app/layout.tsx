7cfc71300ff4e0a33890116902beb10074249875
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 18:00:42 +0600

    Fixed import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 
-import '@/app/global.css';
+import '@/app/globals.css';
 
 import HeaderSection from '@/sections/header-section/HeaderSection';
 import FooterSection from '@/sections/footer-section/FooterSection';
