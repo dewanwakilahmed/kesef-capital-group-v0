bb732a047113d9716028309e58e3c15c8d996dc9
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-26 02:18:44 +0600

    Created Why Choose Us Section Shell

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import '@/app/home-page.css';
 import AboutUsSection from '@/sections/home-page/about-us-section/AboutUsSection';
 import FinancialSolutionsOverviewSection from '@/sections/home-page/financial-solutions-overview-section/FinancialSolutionsOverviewSection';
 import HeroSection from '@/sections/home-page/hero-section/HeroSection';
+import WhyChooseUsSection from '@/sections/home-page/why-choose-us-section/WhyChooseUsSection';
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <HeroSection />
       <AboutUsSection />
       <FinancialSolutionsOverviewSection />
+      <WhyChooseUsSection />
     </div>
   );
 };
---
+++ sections/home-page/why-choose-us-section/WhyChooseUsSection.tsx
@@ -0,0 +1,9 @@
+import '@/sections/home-page/why-choose-us-section/why-choose-us-section.css';
+
+const WhyChooseUsSection = () => {
+  return (
+    <section className='why-choose-us-section'>WhyChooseUsSection</section>
+  );
+};
+
+export default WhyChooseUsSection;
