64c4dcfe1b6bf997ef0f6f20c6b143c7c5cc6a63
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-26 02:24:34 +0600

    Created Testimonials Section Shell

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import '@/app/home-page.css';
 import AboutUsSection from '@/sections/home-page/about-us-section/AboutUsSection';
 import FinancialSolutionsOverviewSection from '@/sections/home-page/financial-solutions-overview-section/FinancialSolutionsOverviewSection';
 import HeroSection from '@/sections/home-page/hero-section/HeroSection';
+import TestimonialsSection from '@/sections/home-page/testimonials-section/TestimonialsSection';
 import WhyChooseUsSection from '@/sections/home-page/why-choose-us-section/WhyChooseUsSection';
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
       <AboutUsSection />
       <FinancialSolutionsOverviewSection />
       <WhyChooseUsSection />
+      <TestimonialsSection />
     </div>
   );
 };
---
+++ sections/home-page/testimonials-section/TestimonialsSection.tsx
@@ -0,0 +1,9 @@
+import '@/sections/home-page/testimonials-section/testimonials-section.css';
+
+const TestimonialsSection = () => {
+  return (
+    <section className='testimonials-section'>TestimonialsSection</section>
+  );
+};
+
+export default TestimonialsSection;
