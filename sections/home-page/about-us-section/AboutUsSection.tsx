134c7dc3169d206af652d994bf62782f6befbf65
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 22:27:26 +0600

    Finished About Us Section

--- sections/home-page/about-us-section/AboutUsSection.tsx
+++ sections/home-page/about-us-section/AboutUsSection.tsx
@@ -2,46 +2,58 @@ import Image from 'next/image';
 
 import '@/sections/home-page/about-us-section/about-us-section.css';
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 const AboutUsSection = () => {
   return (
     <section className='about-us-section'>
-      <h2 className='about-us-section-title'>About Us</h2>
-
-      <div className='about-us-section-content'>
-        <div className='company-story-container'>
-          <Image
-            src='/images/friendly-face.jpg'
-            alt='friendly-face'
-            width={225}
-            height={150}
-            className='about-us-section-img'
-          />
-
-          <p className='company-story'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Our Story</CardTitle>
+          <CardDescription>
             Welcome to Powerhouse Funding, where financial empowerment meets
             entrepreneurial dreams. Founded in 2024, our journey has been a
             testament to resilience and innovation in providing tailored
             financial solutions for small businesses.
-          </p>
-        </div>
-
-        <div className='company-mission-statement-container'>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
           <Image
             src='/images/small-business.jpg'
             alt='small-business'
-            width={225}
-            height={100}
+            width={300}
+            height={200}
             className='about-us-section-img'
           />
+        </CardContent>
+      </Card>
 
-          <p className='company-mission-statement'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Our Mission</CardTitle>
+          <CardDescription>
             At Powerhouse Funding, our mission is to break down financial
             barriers, offering not just funding but a genuine partnership in the
             success of small businesses. We believe in empowering entrepreneurs
             to thrive and grow, contributing to the prosperity of communities.
-          </p>
-        </div>
-      </div>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Image
+            src='/images/friendly-face.jpg'
+            alt='friendly-face'
+            width={300}
+            height={200}
+            className='about-us-section-img'
+          />
+        </CardContent>
+      </Card>
     </section>
   );
 };
