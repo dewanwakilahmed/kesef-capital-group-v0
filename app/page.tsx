70c13c3924da198b9302e368be85952884b485f3
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 16:59:52 +0600

    Created Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import HeroSection from '@/sections/home-page/hero-section/HeroSection';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='home-page'>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ sections/home-page/hero-section/HeroSection.tsx
@@ -0,0 +1,7 @@
+import '@/sections/home-page/hero-section/hero-section.css';
+
+const HeroSection = () => {
+  return <section className='hero-section'>HeroSection</section>;
+};
+
+export default HeroSection;
