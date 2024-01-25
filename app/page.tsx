efc2465a8f8a92f291de0969779f02131e05ea82
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-26 02:15:08 +0600

    Created Financial Solutions Overview Section Shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import '@/app/home-page.css';
 
 import AboutUsSection from '@/sections/home-page/about-us-section/AboutUsSection';
+import FinancialSolutionsOverviewSection from '@/sections/home-page/financial-solutions-overview-section/FinancialSolutionsOverviewSection';
 import HeroSection from '@/sections/home-page/hero-section/HeroSection';
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <div className='home-page'>
       <HeroSection />
       <AboutUsSection />
+      <FinancialSolutionsOverviewSection />
     </div>
   );
 };
---
+++ sections/home-page/financial-solutions-overview-section/FinancialSolutionsOverviewSection.tsx
@@ -0,0 +1,11 @@
+import '@/sections/home-page/financial-solutions-overview-section/financial-solutions-overview-section.css';
+
+const FinancialSolutionsOverviewSection = () => {
+  return (
+    <section className='financial-solutions-overview-section'>
+      FinancialSolutionsOverviewSection
+    </section>
+  );
+};
+
+export default FinancialSolutionsOverviewSection;
