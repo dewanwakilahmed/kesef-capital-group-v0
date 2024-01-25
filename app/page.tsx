d9ab6cd15d1ba94b2f5ea8e863e184767f53cfe3
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 19:30:31 +0600

    Added About Us Section Shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import '@/app/home-page.css';
 
+import AboutUsSection from '@/sections/home-page/about-us-section/AboutUsSection';
 import HeroSection from '@/sections/home-page/hero-section/HeroSection';
 
 const HomePage = () => {
   return (
     <div className='home-page'>
       <HeroSection />
+      <AboutUsSection />
     </div>
   );
 };
---
+++ sections/home-page/about-us-section/AboutUsSection.tsx
@@ -0,0 +1,7 @@
+import '@/sections/home-page/about-us-section/about-us-section.css';
+
+const AboutUsSection = () => {
+  return <section className='about-us-section'>AboutUsSection</section>;
+};
+
+export default AboutUsSection;
