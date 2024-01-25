cb9e0f2ddbbf3969bc46d16b2921f8127629ab7a
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-25 13:28:48 +0600

    Created RootLayoutProps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { FC, ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
+
 import './globals.css';
 
 const customFont = Montserrat({ subsets: ['latin'] });
@@ -10,14 +12,16 @@ export const metadata: Metadata = {
     'Empowering small businesses with short-term financial solutions',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface RootLayoutProps {
+  readonly children: ReactNode;
+}
+
+const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang='en'>
       <body className={customFont.className}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
