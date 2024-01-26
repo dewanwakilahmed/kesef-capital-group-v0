0da11d8e69661eaa6a1eb838f496e578c075dd4a
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-26 20:47:54 +0600

    Modified about us section of home page

--- sections/home-page/about-us-section/AboutUsSection.tsx
+++ sections/home-page/about-us-section/AboutUsSection.tsx
@@ -5,29 +5,29 @@ import './about-us-section.css';
 const AboutUsSection = () => {
   return (
     <section className='about-us-section'>
-      <h2 className='about-us-section-title'>About Us</h2>
+      <h2 className='about-us-title'>About Us</h2>
 
-      <div className='about-us-section-content'>
-        <div className='custom-card'>
-          <h3 className='card-title'>Our Story</h3>
-          <p className='card-description'>
+      <div className='about-us-content'>
+        <div className='about-us-card'>
+          <h3 className='about-us-card-title'>Our Story</h3>
+          <p className='about-us-card-description'>
             Welcome to Powerhouse Funding, where financial empowerment meets
-            entrepreneurial dreams. Founded in 2024, our journey has been a
-            testament to resilience and innovation in providing tailored
-            financial solutions for small businesses.
+            entrepreneurial dreams. Established in 2024, our journey reflects
+            resilience and innovation in providing tailored financial solutions
+            for small businesses.
           </p>
           <Image
             src='/images/small-business.jpg'
             alt='small-business'
             width={300}
             height={200}
-            className='about-us-section-img'
+            className='about-us-card-img'
           />
         </div>
 
-        <div className='custom-card'>
-          <h3 className='card-title'>Our Mission</h3>
-          <p className='card-description'>
+        <div className='about-us-card'>
+          <h3 className='about-us-card-title'>Our Mission</h3>
+          <p className='about-us-card-description'>
             At Powerhouse Funding, our mission is to break down financial
             barriers, offering not just funding but a genuine partnership in the
             success of small businesses. We believe in empowering entrepreneurs
@@ -38,7 +38,7 @@ const AboutUsSection = () => {
             alt='friendly-face'
             width={300}
             height={200}
-            className='about-us-section-img'
+            className='about-us-card-img'
           />
         </div>
       </div>
