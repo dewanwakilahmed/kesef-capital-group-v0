e812d714ca89a59a267a6d117cb9290547fe2c05
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-26 14:38:18 +0600

    Changed Section Name

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import '@/app/home-page.css';
 
 import HeroSection from '@/sections/home-page/hero-section/HeroSection';
 import AboutUsSection from '@/sections/home-page/about-us-section/AboutUsSection';
-import FinancialSolutionsOverviewSection from '@/sections/home-page/financial-solutions-overview-section/FinancialSolutionsOverviewSection';
+import OurFinancialSolutionsSection from '@/sections/home-page/our-financial-solutions-section/OurFinancialSolutionsSection';
 import WhyChooseUsSection from '@/sections/home-page/why-choose-us-section/WhyChooseUsSection';
 import TestimonialsSection from '@/sections/home-page/testimonials-section/TestimonialsSection';
 import CallToActionSection from '@/sections/home-page/call-to-action-section/CallToActionSection';
@@ -12,7 +12,7 @@ const HomePage = () => {
     <div className='home-page'>
       <HeroSection />
       <AboutUsSection />
-      <FinancialSolutionsOverviewSection />
+      <OurFinancialSolutionsSection />
       <WhyChooseUsSection />
       <TestimonialsSection />
       <CallToActionSection />
---
+++ sections/home-page/our-financial-solutions-section/OurFinancialSolutionsSection.tsx
@@ -0,0 +1,13 @@
+import '@/sections/home-page/our-financial-solutions-section/our-financial-solutions-section.css';
+
+const OurFinancialSolutionsSection = () => {
+  return (
+    <section className='our-financial-solutions-section'>
+      <h2 className='section-title'>Our Financial Solutions</h2>
+
+      <div className='our-financial-solutions-section-content'>Content</div>
+    </section>
+  );
+};
+
+export default OurFinancialSolutionsSection;
