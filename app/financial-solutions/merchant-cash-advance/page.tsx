08af27d22265376ccbda5b078bea966412ed896d
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-27 20:37:32 +0600

    Created Merchant Cash Advance Page

--- app/financial-solutions/merchant-cash-advance/page.tsx
+++ app/financial-solutions/merchant-cash-advance/page.tsx
@@ -1,5 +1,14 @@
-const MerchantCashAdvancePage = () => {
-  return <div>MerchantCashAdvancePage</div>;
+'use client';
+
+import { FC } from 'react';
+
+// Import CSS
+import './merchant-cash-advance-page.css';
+
+const MerchantCashAdvancePage: FC = () => {
+  return (
+    <div className='merchant-cash-advance-page'>MerchantCashAdvancePage</div>
+  );
 };
 
 export default MerchantCashAdvancePage;
