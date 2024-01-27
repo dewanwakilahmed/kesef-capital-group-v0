ed0d649569d8a9860ad47f656de9d68972fefdee
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-27 20:58:53 +0600

    Created Merchant Cash Advance Hero section

---
+++ sections/financial-solutions-page/merchant-cash-advance-page/merchant-cash-advance-hero-section/MerchantCashAdvanceHeroSection.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { FC } from 'react';
+
+// Import CSS
+import './merchant-cash-advance-hero-section.css';
+
+const MerchantCashAdvanceHeroSection: FC = () => {
+  return (
+    <section className='merchant-cash-advance-hero-section'>
+      MerchantCashAdvanceHeroSection
+    </section>
+  );
+};
+
+export default MerchantCashAdvanceHeroSection;
