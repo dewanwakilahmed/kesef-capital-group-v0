831a0d24f2ccb3c8f8700a9c293fe8b1d588d736
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-28 22:09:43 +0600

    Created Asset Based Lending Hero section

--- app/financial-solutions/asset-based-lending/page.tsx
+++ app/financial-solutions/asset-based-lending/page.tsx
@@ -1,5 +1,19 @@
-const AssetBasedLendingPage = () => {
-  return <div>AssetBasedLendingPage</div>;
+'use client';
+
+import { FC } from 'react';
+
+// Import CSS
+import './asset-based-lending-page.css';
+
+// Import Sections
+import AssetBasedLendingHeroSection from '@/sections/financial-solutions-page/asset-based-lending-page/asset-based-lending-hero-section/AssetBasedLendingHeroSection';
+
+const AssetBasedLendingPage: FC = () => {
+  return (
+    <div className='asset-based-lending-page'>
+      <AssetBasedLendingHeroSection />
+    </div>
+  );
 };
 
 export default AssetBasedLendingPage;
