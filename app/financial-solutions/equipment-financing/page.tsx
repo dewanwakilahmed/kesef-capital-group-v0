bc1c18bdfc6852dfac788d36852eb5bf11d36f7f
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-28 18:37:20 +0600

    Created Equipment Financing Hero section

--- app/financial-solutions/equipment-financing/page.tsx
+++ app/financial-solutions/equipment-financing/page.tsx
@@ -1,5 +1,19 @@
-const EquipmentFinancingPage = () => {
-  return <div>EquipmentFinancingPage</div>;
+'use client';
+
+import { FC } from 'react';
+
+// Import CSS
+import './equipment-financing-page.css';
+
+// Import Sections
+import EquipmentFinancingHeroSection from '@/sections/financial-solutions-page/equipment-financing-page/equipment-financing-hero-section/EquipmentFinancingHeroSection';
+
+const EquipmentFinancingPage: FC = () => {
+  return (
+    <div className='equipment-financing-page'>
+      <EquipmentFinancingHeroSection />
+    </div>
+  );
 };
 
 export default EquipmentFinancingPage;
