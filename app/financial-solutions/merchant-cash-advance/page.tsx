58fe01850096904074feeec9138f6edef62be87a
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-28 16:58:47 +0600

    Created Merchant Cash Advance Hero section

--- app/financial-solutions/merchant-cash-advance/page.tsx
+++ app/financial-solutions/merchant-cash-advance/page.tsx
@@ -6,12 +6,14 @@ import { FC } from 'react';
 import './merchant-cash-advance-page.css';
 
 // Import Sections
+import MerchantCashAdvanceHeroSection from '@/sections/financial-solutions-page/merchant-cash-advance-page/merchant-cash-advance-hero-section/MerchantCashAdvanceHeroSection';
 import WhatIsMerchantCashAdvanceSection from '@/sections/financial-solutions-page/merchant-cash-advance-page/what-is-merchant-cash-advance-section/WhatIsMerchantCashAdvanceSection';
 
 const MerchantCashAdvancePage: FC = () => {
   return (
     <div className='merchant-cash-advance-page'>
-      <WhatIsMerchantCashAdvanceSection />
+      <MerchantCashAdvanceHeroSection />
+      {/* <WhatIsMerchantCashAdvanceSection /> */}
     </div>
   );
 };
--- sections/financial-solutions-page/merchant-cash-advance-page/merchant-cash-advance-hero-section/MerchantCashAdvanceHeroSection.tsx
+++ sections/financial-solutions-page/merchant-cash-advance-page/merchant-cash-advance-hero-section/MerchantCashAdvanceHeroSection.tsx
@@ -5,10 +5,32 @@ import { FC } from 'react';
 // Import CSS
 import './merchant-cash-advance-hero-section.css';
 
+// Import Component
+import Button from '@/components/ui/button/Button';
+
 const MerchantCashAdvanceHeroSection: FC = () => {
   return (
     <section className='merchant-cash-advance-hero-section'>
-      MerchantCashAdvanceHeroSection
+      <div className='merchant-cash-advance-hero-bg-img-overlay' />
+
+      <div className='merchant-cash-advance-hero-content'>
+        <h1 className='merchant-cash-advance-hero-headline'>
+          Funding Your Way: Tailored Solutions for Every Business Dream
+        </h1>
+
+        <p className='merchant-cash-advance-hero-subheadline'>
+          Unlock your ambition. Scale your dreams. Partner with Powerhouse
+          Funding!
+        </p>
+
+        <Button
+          className='merchant-cash-advance-hero-cta'
+          btnType='primary'
+          goTo='/financial-solutions'
+        >
+          Apply Now
+        </Button>
+      </div>
     </section>
   );
 };
