71e897455e13d6a8e7f9ae4edfa6b1162ab2b195
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-29 19:07:27 +0600

    Deleted unused backgroundImages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,22 +12,6 @@ const config: Config = {
       colors: {
         primary: '#3b82f6',
       },
-      backgroundImage: {
-        'home-hero': "url('/images/hero-images/home-hero.jpg')",
-        'merchant-cash-advance-hero':
-          "url('/images/hero-images/merchant-cash-advance-hero.webp')",
-        'term-loan-hero': "url('/images/hero-images/term-loan-hero.webp')",
-        'line-of-credit-hero':
-          "url('/images/hero-images/line-of-credit-hero.webp')",
-        'bad-credit-lending-hero':
-          "url('/images/hero-images/bad-credit-lending-hero.webp')",
-        'asset-based-lending-hero':
-          "url('/images/hero-images/asset-based-lending-hero.webp')",
-        'equipment-financing-hero':
-          "url('/images/hero-images/equipment-financing-hero.webp')",
-        'financial-success': "url('/images/financial-success.jpg')",
-        'office-workplace': "url('/images/office-workplace.jpg')",
-      },
       backgroundColor: {
         overlay: 'rgba(0, 0, 0, 0.75)',
       },
